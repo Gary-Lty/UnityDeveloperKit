888b59527657738a9dd33da672169bb3eccf776b
DayAndStar <987918453@qq.com>
2022-06-18 00:51:09 +0800

    add：对象池

---
+++ Assets/lty/PoolTest.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using DeveloperKit.Runtime.Pool;
+using Sirenix.OdinInspector;
+using UnityDeveloperKit.Runtime.Api;
+using UnityEngine;
+
+namespace UnityDeveloperKit.Tests
+{
+
+    public interface IDesk : IComponent,ICanRecycle,IHasCreator<IDesk>
+    {
+        
+    }
+    
+    public class PoolTest : MonoBehaviour
+    {
+        [System.Serializable]
+        public class DeskPool : ComponentPool<IDesk>
+        {
+            public Transform parent;
+            public override IDesk NewItem()
+            {
+                var t = GameObject.Instantiate(Prefab).GetComponent<IDesk>();
+                t.Creator = this;
+                return t;
+            }
+        }
+
+        public DeskPool deskPool = new DeskPool();
+
+        public List<GameObject> poolObjs;
+        [Button]
+        public void Pop()
+        {
+            deskPool.PopItem();
+
+            poolObjs = new List<GameObject>();
+            foreach (var desk in deskPool.Content)
+            {
+                poolObjs.Add(desk.gameObject);
+            }
+        }
+        
+    }
+}
---
+++ Assets/lty/TopDesk.cs
@@ -0,0 +1,22 @@
+﻿using DeveloperKit.Runtime.Pool;
+using Sirenix.OdinInspector;
+using UnityEngine;
+
+namespace UnityDeveloperKit.Tests
+{
+    public class TopDesk : MonoBehaviour,IDesk
+    {
+        public bool IsInUse { get; set; }
+        public void OnRecycle()
+        {
+            Debug.Log("OnRecycle");
+        }
+        public ICanRecycleItem<IDesk> Creator { get; set; }
+
+        [Button]
+        public void R()
+        {
+            this.Recycle();
+        }
+    }
+}
